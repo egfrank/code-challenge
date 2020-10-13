1e36f24ad8b20ce6abf101b95acc602c8838cade
Elliot Frank <elliot.g.frank@gmail.com>
2020-10-13 09:28:44 -0500

    Pass url params to django client get

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,13 +1,12 @@
-import urllib.parse
-
 from django.urls import reverse
 
 
 def test_api_parse_success(client, success_response):
     address_string = "123 main st chicago il"
-    url = (reverse("address-parse") + "?address="
-           + urllib.parse.quote(address_string))
-    response = client.get(url)
+    response = client.get(
+        reverse("address-parse"),
+        {"address": address_string}
+    )
 
     assert response.status_code == 200
     assert response.json() == success_response
@@ -15,9 +14,9 @@ def test_api_parse_success(client, success_response):
 
 def test_api_parse_raise_error(client, error_response):
     address_string = "123 main st chicago il 123 main st"
-    url = (reverse("address-parse") + "?address="
-           + urllib.parse.quote(address_string))
-    response = client.get(url)
-
+    response = client.get(
+        reverse("address-parse"),
+        {"address": address_string}
+    )
     assert response.status_code == 400
     assert response.json() == error_response
